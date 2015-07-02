84d581ba3d3fa867f6ee1745c59b2479627f079e
Nicholas Young <nicholas@nicholaswyoung.com>
2015-07-02 12:50:01 -0500

    Minimal map.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,12 @@
-console.log('Hey!')
+document.addEventListener('DOMContentLoaded', function () {
+  var mapElement = document.querySelector('.map');
+
+  if (mapElement) {
+    var map = new google.maps.Map(mapElement, {
+      center: new google.maps.LatLng(41.890775, -87.628441),
+      zoom: 16,
+      disableDefaultUI: true,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+  }
+});
