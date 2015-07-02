b70fbbbcfd905b2f1923a383e2c7199633b9a24e
Nicholas Young <nicholas@nicholaswyoung.com>
2015-07-02 13:27:21 -0500

    Add map markers.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,35 @@
 document.addEventListener('DOMContentLoaded', function () {
-  var mapElement = document.querySelector('.map');
+  /**
+   * Setup
+   */
+  var mapElement  = document.querySelector('.map')
+  ,   infoElement = document.querySelector('.map-info');
+  if (!mapElement || !infoElement) return;
 
-  if (mapElement) {
-    var map = new google.maps.Map(mapElement, {
-      center: new google.maps.LatLng(41.890775, -87.628441),
-      zoom: 16,
-      disableDefaultUI: true,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-    });
-  }
+  /**
+   * Build Map
+   */
+   var location = new google.maps.LatLng(41.890775, -87.628441);
+
+   var map = new google.maps.Map(mapElement, {
+     center: location,
+     zoom: 16,
+     disableDefaultUI: true,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+   });
+
+   var infoWindow = new google.maps.InfoWindow({
+     content: infoElement.innerHTML
+   });
+
+   var marker = new google.maps.Marker({
+     position: location,
+     map: map,
+     title: 'Earhole Studios',
+     animation: google.maps.Animation.DROP
+   });
+
+   google.maps.event.addListener(marker, 'click', function () {
+     infoWindow.open(map, marker);
+   });
 });
