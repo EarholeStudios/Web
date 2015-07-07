95ad68fb7338f962d26aee9ded96936c627ca0b9
Nicholas Young <nicholas@nicholaswyoung.com>
2015-07-07 12:41:25 -0500

    Front page is done.

--- src/assets/js/includes/index.js
+++ src/assets/js/includes/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+  var touchEnabled = 'ontouchstart' in document.documentElement;
+
   if (!$('body').hasClass('index')) return;
+  if (touchEnabled) return;
 
   /**
    * Setup
@@ -24,72 +27,123 @@ $(document).ready(function () {
     });
   });
 
+  var utils = {
+    copy: function (obj) {
+      if (null === obj || 'object' !== typeof obj) return obj;
+      var copy = obj.constructor();
+      for (var attr in obj) {
+        if (obj.hasOwnProperty(attr)) copy[attr] = obj[attr];
+      }
+      return copy;
+    }
+  };
+
   /**
-   * Build a global events bus.
+   * Build a global event `Controller`.
    */
-  var eventBus = {
+  var Controller = {
     state: {
       sampleIndex: 0,
+      soundtrackIndex: 0,
       objectCount: 0,
       activated: false
     },
-    init: function () {
-      this.defaultState = this.state;
+
+    /**
+     * State helpers.
+     */
+    initialize: function () {
+      this.defaultState = utils.copy(this.state);
     },
+
     getState: function (key) {
       return this.state[key];
     },
+
     setState: function (key, value) {
       this.state[key] = value;
     },
+
     incrementState: function (key) {
-      if ('number' !== this.state[key]) return;
+      if ('number' !== typeof this.state[key]) return;
       this.setState(key, (this.state[key] + 1));
     },
+
     resetState: function (key) {
       if (key) return this.state[key] = this.defaultState[key];
-      this.state = this.defaultState;
+      this.state = utils.copy(this.defaultState);
     },
-    addCircle: function () {
+
+    addCircle: function (event) {
       var container = $('.playground')
       ,   element   = $(document.createElement('div'));
 
-      element.addClass('object')
-      element.addClass(['object', this.getState('objectCount')].join('-'))
-      element.dot(event.pageX, event.pageY, false, true);
+      element
+        .addClass('object')
+        .addClass(['object', this.getState('objectCount')].join('-'))
+        .dot(event.pageX, event.pageY, false, true);
 
+      this.incrementState('objectCount');
       container.append(element);
     },
-    fireSample: function (index) {
-      index = index || 0;
-      if (index > samples.length) index = (samples.length - 1);
-      samples[index].play();
+
+    handleClick: function (event) {
+      if (this.getState('soundtrackIndex') > soundtrack.length) {
+        this.setState('soundtrackIndex', 0);
+      }
+
+      if (this.getState('sampleIndex') > samples.length) {
+        this.setState('sampleIndex', 0);
+      }
+
+      if (!this.getState('activated')) {
+        this.setState('activated', true);
+        $('.controls.close').show();
+      }
+
+      var backbeat = soundtrack[this.getState('soundtrackIndex')]
+      ,   sample   = samples[this.getState('sampleIndex')];
+
+      if (backbeat) backbeat.play();
+      if (sample) sample.play();
+
+      if (!this.currentSoundtrack) this.currentSoundtrack = backbeat;
+
+      this.incrementState('soundtrackIndex');
+      this.incrementState('sampleIndex');
+      this.setState('activated', true);
+
+      this.addCircle(event);
     },
-    fireSoundtrack: function (index) {
-      index = index || 0;
-      if (index > soundtrack.length) index = (soundtrack.length - 1);
-      if (!this.getState('activated')) return;
-      soundtrack[index].play();
-      firstClick = false;
-    },
+
     cleanup: function () {
+      if (this.currentSoundtrack) {
+        this.currentSoundtrack.stop();
+        this.currentSoundtrack = null;
+      }
+
       $('.playground .object').hide().remove();
       $('.controls.close').hide();
+
+      this.resetState();
     }
   };
+
+  /**
+   * Initialize `eventbus`
+   */
+  Controller.initialize();
 
   /**
    * Fire events on `click`, delegating events to the `eventsBus`
    */
   $('.playground').click(function (event) {
     event.preventDefault();
-    eventBus.addCircle();
-    eventBus.fireSoundtrack();
-    eventBus.fireSample();
+    Controller.handleClick(event);
   });
 
   $('.controls.close').click(function (event) {
     event.preventDefault();
-    eventBus.cleanup();
+    Controller.cleanup();
   });
 });
